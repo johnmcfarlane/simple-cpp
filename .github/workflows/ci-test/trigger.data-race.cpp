f4531466d3e65a3205d6ce24cd271e5342f6796a
John McFarlane <git@john.mcfarlane.name>
2026-03-15 03:40:14 +0000

    Name labels more clearly
    - to show that they are arbitrary
    - and address Clang-Tidy diagnostics.

--- .github/workflows/ci-test/trigger.data-race.cpp
+++ .github/workflows/ci-test/trigger.data-race.cpp
@@ -4,6 +4,6 @@
 auto main() noexcept(false) -> int
 {
     auto shared_state{0};
-    std::jthread thread{[&] { shared_state = 1; }};
+    std::jthread const thread{[&] { shared_state = 1; }};
     std::print("{}\n", shared_state);
 }
