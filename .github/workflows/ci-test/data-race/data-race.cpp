d2035d9e109a5a2f189bc67019dcb6c6df828a7c
John McFarlane <git@john.mcfarlane.name>
2026-04-05 12:53:16 +0100

    Clean up include usage
    - and avoid std::print in favour of fmt::print.

--- .github/workflows/ci-test/data-race/data-race.cpp
+++ .github/workflows/ci-test/data-race/data-race.cpp
@@ -1,9 +1,10 @@
-#include <print>
+#include <fmt/printf.h>
+
 #include <thread>
 
 auto main() -> int
 {
     auto shared_state{0};
     std::jthread const thread{[&] { shared_state = 1; }};
-    std::print("{}\n", shared_state);
+    fmt::print("{}\n", shared_state);
 }
