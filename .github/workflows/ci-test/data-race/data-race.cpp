f3c1cca0cc682c88989cec94b220961376980b91
John McFarlane <git@john.mcfarlane.name>
2026-04-05 12:53:16 +0100

    Clean up include usage

--- .github/workflows/ci-test/data-race/data-race.cpp
+++ .github/workflows/ci-test/data-race/data-race.cpp
@@ -1,9 +1,8 @@
-#include <print>
 #include <thread>
 
 auto main() -> int
 {
     auto shared_state{0};
     std::jthread const thread{[&] { shared_state = 1; }};
-    std::print("{}\n", shared_state);
+    return shared_state;
 }
--- src/simple.unit-test.cpp
+++ src/simple.unit-test.cpp
@@ -1,5 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include <limits>
+
 import simple;
 
 TEST_CASE("non-negative", "simple::add")
